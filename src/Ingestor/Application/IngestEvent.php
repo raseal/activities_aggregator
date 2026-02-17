f0bf3b94e31047b56726a03b4aed5093e72616fc
raseal <hiko.777@gmail.com>
2026-02-17 16:21:00 +0100

    Add IngestEvent class for event ingestion and error handling

---
+++ src/Ingestor/Application/IngestEvent.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ingestor\Application;
+
+use Exception;
+use Ingestor\Application\DTO\Event as EventDTO;
+use Ingestor\Domain\EventRepository;
+use Psr\Log\LoggerInterface;
+use Shared\Application\Bus\Event\EventBus;
+use Throwable;
+
+final readonly class IngestEvent
+{
+    public function __construct(
+        private EventFactory $eventFactory,
+        private EventRepository $eventRepository,
+        private LoggerInterface $logger,
+        private EventBus $eventBus,
+    ) {}
+
+    public function __invoke(EventDTO $eventDto): void
+    {
+        try {
+            $event = $this->eventFactory->fromDTO($eventDto);
+            $this->eventRepository->save($event);
+            $this->eventBus->publish(...$event->pullDomainEvents());
+        } catch (Throwable $exception) {
+            $this->logger->error('Failed to ingest event', [
+                'event_id' => $eventDto->eventId,
+                'base_event_id' => $eventDto->baseEvent->baseEventId,
+                'error_message' => $exception->getMessage(),
+                'error_class' => get_class($exception),
+                'error_trace' => $exception->getTraceAsString(),
+            ]);
+
+            throw new Exception('Failed to ingest event', previous: $exception);
+        }
+    }
+}
